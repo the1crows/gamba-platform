effd8df63ee7632ff980ba68b13dc877cb1c74dd
the1crows <113790120+the1crows@users.noreply.github.com>
2024-02-26 00:43:46 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import { PublicKey } from '@solana/web3.js'
 export const RPC_ENDPOINT = "https://mainnet.helius-rpc.com/?api-key=4746c733-040f-4e41-9835-c20b26d4ec67"
 
 // Change this value to your Solana address
-export const PLATFORM_CREATOR_ADDRESS = new PublicKey('GvKUkTd13Ge7htV61W46j89ytQwnotiyrCPRmFZiuLb7')
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey('E8tzfzDJa2CVc2BVXu6mmg83sp2JV4mXeVTvR5GwCJyu')
 
 // Appears in ShareModal
 export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
