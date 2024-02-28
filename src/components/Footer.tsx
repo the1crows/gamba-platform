c605850a277d8931e5b12d3ca3780e50a01676be
crow <113790120+the1crows@users.noreply.github.com>
2024-02-28 08:57:10 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+import { FooterContainer, FooterText } from './FooterStyles'; // Adjust the path as necessary
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>© 2024 Your Company Name. All rights reserved.</FooterText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
