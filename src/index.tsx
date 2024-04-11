601928e75c34a541eff7e42b1d9bf0090270bd77
crow <113790120+the1crows@users.noreply.github.com>
2024-04-12 00:48:50 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import '@solana/wallet-adapter-react-ui/styles.css'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
-import { GambaPlatformProvider } from 'gamba-react-ui-v2'
-import { GambaProvider } from 'gamba-react-v2'
+import { GambaPlatformProvider, TokenMetaProvider } from 'gamba-react-ui-v2'
+import { GambaProvider, SendTransactionProvider } from 'gamba-react-v2'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { PLATFORM_CREATOR_ADDRESS, RPC_ENDPOINT, TOKENS } from './constants'
-import { GAMES } from './games'
+import { DEFAULT_POOL, PLATFORM_CREATOR_ADDRESS, PLATFORM_CREATOR_FEE, PLATFORM_JACKPOT_FEE, RPC_ENDPOINT, TOKEN_METADATA, TOKEN_METADATA_FETCHER } from './constants'
 import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
@@ -31,17 +30,23 @@ function Root() {
       >
         <WalletProvider autoConnect wallets={wallets}>
           <WalletModalProvider>
-            <GambaProvider>
-              <GambaPlatformProvider
-                creator={PLATFORM_CREATOR_ADDRESS}
-                games={GAMES}
-                tokens={TOKENS}
-                defaultCreatorFee={0.05} //5%
-                defaultJackpotFee={0.01} //1%
-              >
-                <App />
-              </GambaPlatformProvider>
-            </GambaProvider>
+            <TokenMetaProvider
+              tokens={TOKEN_METADATA}
+              fetcher={TOKEN_METADATA_FETCHER}
+            >
+              <SendTransactionProvider priorityFee={400_201}>
+                <GambaProvider>
+                  <GambaPlatformProvider
+                    creator={PLATFORM_CREATOR_ADDRESS}
+                    defaultPool={DEFAULT_POOL}
+                    defaultCreatorFee={PLATFORM_CREATOR_FEE}
+                    defaultJackpotFee={PLATFORM_JACKPOT_FEE}
+                  >
+                    <App />
+                  </GambaPlatformProvider>
+                </GambaProvider>
+              </SendTransactionProvider>
+            </TokenMetaProvider>
           </WalletModalProvider>
         </WalletProvider>
       </ConnectionProvider>
