48a2b991852cfc65f750094b78ed17420974fc95
crow <113790120+the1crows@users.noreply.github.com>
2024-04-27 16:43:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,6 @@ export default function App() {
           </Routes>
           <h2 style={{ textAlign: 'center' }}>Recent Plays</h2>
           <RecentPlays />
-          <Footer />
         </StyledSection>
       </div> 
     </>
